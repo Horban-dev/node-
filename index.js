a63646ab826d028902d110d8b581af899ac420f0
Illia <illia.horban@gmail.com>
2023-10-08 17:03:53 +0300

    add and setting google auth

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
+passport.use(new GoogleStrategy({
+    clientID: '896819412048-9pgi251l85hvohgdrl1mknokk388g8m0.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile) => {
+    console.log(accessToken);
+    console.log(refreshToken)
+    console.log(profile)
 
-const port = 4000
-app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({Illia: 'hello illia'})
-})
+}));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/'); 
+  });
 
 
-app.listen(port)
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
