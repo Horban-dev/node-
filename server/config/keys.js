c25b6978020b57d33dba5079f51c723fbc0222d3
Illia <illia.horban@gmail.com>
2023-10-17 12:40:52 +0300

    finished login/logout

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {  
     googleClientID: '896819412048-9pgi251l85hvohgdrl1mknokk388g8m0.apps.googleusercontent.com',
     googleClientSecret: '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',
-    mongoURI: "mongodb+srv://illia-node:Azov1488@cluster0.4faoj3g.mongodb.net/?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://illia-node:Azov1488@cluster0.4faoj3g.mongodb.net/?retryWrites=true&w=majority",
+    cookieKey: 'ldskjfgfhwuytwqgsbznf'
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 require('./models/User')
 require('./services/passport')
 
 const authRoutes = require('./routes/AuthRoutes')
 const app = express();
+
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+
+app.use(passport.session())
 
 mongoose.connect(keys.mongoURI)
   .then(() => {
